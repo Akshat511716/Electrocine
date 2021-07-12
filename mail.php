ffcc58cdf9e59f1a2d3820831e728efeec67213b
Akshat-Rana <akshat511716@gmail.com>
2021-07-12 11:22:41 +0530

    Updated the email id

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@
 $name = $_POST['name'];
 $email= $_POST['email'];
 $message= $_POST['message'];
-$to = "khushi.sharma.2201.com";
+$to = "akshat511716@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
